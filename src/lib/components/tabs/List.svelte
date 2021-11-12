9d19d25c5b684c5a41a2400f2861548849b55a77
Shawn Lee <changoloco.evg@gmail.com>
2021-11-12 17:23:08 -0600

    create tabs list component

---
+++ src/lib/components/tabs/List.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { isTabsContext, TABS_CONTEXT_KEY } from './Group.svelte';
+
+	let className = '';
+
+	export { className as class };
+	export let active = '';
+	export let unactive = '';
+
+	const TabsContext = getContext(TABS_CONTEXT_KEY);
+	if (!isTabsContext(TabsContext)) throw new Error('Invalid Tabs Context');
+
+	const { tabs } = TabsContext;
+</script>
+
+<div class={className} use:tabs={{ if: active, else: unactive }}>
+	<slot />
+</div>
