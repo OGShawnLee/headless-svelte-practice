d9930a867d714024945a2d8922a76c368f1fc079
Shawn Lee <changoloco.evg@gmail.com>
2021-11-12 09:24:51 -0600

    create tabs tab component

---
+++ src/lib/components/tabs/Tab.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	export { default as TabGroup } from './Group.svelte';
+	export { default as TabPanel } from './Panel.svelte';
+</script>
+
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { isTabsContext, TABS_CONTEXT_KEY } from './Group.svelte';
+
+	let [selected, notifySelected] = [false, (bool: boolean) => (selected = bool)];
+
+	const TabsContext = getContext(TABS_CONTEXT_KEY);
+	if (!isTabsContext(TabsContext)) throw Error('Invalid Tabs Context');
+
+	const { tab } = TabsContext;
+</script>
+
+<button use:tab={notifySelected}>
+	<slot {selected} />
+</button>
