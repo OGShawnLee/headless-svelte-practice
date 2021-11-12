a3abc3150596c89c075f7c051f5c1be4b41d4ad7
Shawn Lee <changoloco.evg@gmail.com>
2021-11-12 09:13:13 -0600

    add basic component configuration

---
+++ src/lib/components/tabs/Group.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+	import { notifiable } from '$lib/stores';
+</script>
+
+<script lang="ts">
+	import { writable } from 'svelte/store';
+
+	export let current = 0;
+	export let manual = false;
+	export let vertical = false;
+	export let onChange: (index: number) => void = () => void 0;
+
+	const Index = notifiable(current, (num) => (current = num));
+	const Manual = writable(manual);
+	const Vertical = writable(vertical);
+
+	$: Index.set(current);
+	$: Vertical.set(vertical);
+	$: Manual.set(manual);
+
+	$: if (onChange) onChange(current);
+</script>
