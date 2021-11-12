29da91eb839bc4ce194078986e6e99d8993caa6b
Shawn Lee <changoloco.evg@gmail.com>
2021-11-12 08:05:41 -0600

    add navigable interface

---
+++ src/lib/types.d.ts
@@ -0,0 +1,34 @@
+import type { Writable } from 'svelte/store';
+
+export interface Navigable {
+	handlers: {
+		handleSelection: (index: number) => () => void;
+		handleKeyboard: (event: KeyboardEvent) => void;
+		handleKeyMatch: (event: KeyboardEvent) => void;
+	};
+	watchers: {
+		watchNavigation: (callback?: {
+			indexCb?: (index: number) => void;
+			manualIndexCb?: (index: number) => void;
+		}) => Unsubscriber;
+		watchSelected: (
+			callback: (selected: HTMLElement, previous?: HTMLElement) => void
+		) => Unsubscriber;
+		watchActive: (
+			callback: (active: HTMLElement, previous?: HTMLElement) => void
+		) => Unsubscriber;
+	};
+	status: {
+		IsSelected: (index: number) => Readable<boolean>;
+	};
+	lifecycle: {
+		onDestroy: (
+			callback: (state: {
+				Index: Writable<number>;
+				ManualIndex: Writable<number>;
+				Waiting: Writable<boolean>;
+				VerticalWaiting: Writable<boolean>;
+			}) => void
+		) => void;
+	};
+}
