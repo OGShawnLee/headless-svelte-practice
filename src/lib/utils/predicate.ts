3efb121bc0cdd9e3945bd2e7636d62341a82f26a
Shawn Lee <changoloco.evg@gmail.com>
2021-11-12 08:26:06 -0600

    add isNumberArray predicate function

--- src/lib/utils/predicate.ts
+++ src/lib/utils/predicate.ts
@@ -5,3 +5,8 @@ export function isBoolean(val: unknown): val is boolean {
 export function isObject(val: unknown): val is object {
 	return val instanceof Object;
 }
+
+export function isNumberArray(val: unknown): val is number[] {
+	if (!(val instanceof Array)) return false;
+	return val.every((val): val is number => typeof val === 'number');
+}
