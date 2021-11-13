8ccd7458768cf03717469e92b1072a7e3314fa0d
Shawn Lee <changoloco.evg@gmail.com>
2021-11-13 11:36:10 -0600

    rename TabList export path

--- src/lib/components/tabs/index.ts
+++ src/lib/components/tabs/index.ts
@@ -1,6 +1,6 @@
 import Tab from './Tab.svelte';
 export { default as TabGroup } from './Group.svelte';
-export { default as TabList } from './LIst.svelte';
+export { default as TabList } from './List.svelte';
 export { default as TabPanels } from './Panels.svelte';
 export { default as TabPanel } from './Panel.svelte';
 
