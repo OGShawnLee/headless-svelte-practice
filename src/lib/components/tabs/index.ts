0d2cd71d85084dcf741f588a44422b7de18eeb04
Shawn Lee <changoloco.evg@gmail.com>
2021-11-12 18:47:58 -0600

    export all tabs components

---
+++ src/lib/components/tabs/index.ts
@@ -0,0 +1,7 @@
+import Tab from './Tab.svelte';
+export { default as TabGroup } from './Group.svelte';
+export { default as TabList } from './LIst.svelte';
+export { default as TabPanels } from './Panels.svelte';
+export { default as TabPanel } from './Panel.svelte';
+
+export default Tab;
