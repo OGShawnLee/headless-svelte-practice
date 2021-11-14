915c61f2c301f40b7739929ff14a6f05a21a1347
Shawn Lee <changoloco.evg@gmail.com>
2021-11-14 14:54:53 -0600

    add useListeners function

--- src/lib/utils/definedListeners.ts
+++ src/lib/utils/definedListeners.ts
@@ -1,3 +1,5 @@
+import type { DefinedListenerBuilder, EventListenerRemover } from '$lib/types';
+
 export function defineListener<E extends Event>(
 	type: string,
 	func: (callback: (event: E) => void, node: HTMLElement) => (e: E) => void,
@@ -8,6 +10,26 @@ export function defineListener<E extends Event>(
 			type,
 			func: func(callback, node),
 			bubble,
+		};
+	};
+}
+
+export function useListeners(target: HTMLElement | Window | Document) {
+	return function (callback: <E extends Event>(event: E) => void, node: HTMLElement) {
+		return function (...listenerBuilders: DefinedListenerBuilder[]) {
+			const builtListeners = listenerBuilders.map((listener) => {
+				return listener(callback, node);
+			});
+
+			builtListeners.forEach(({ type, func, bubble = false }) => {
+				target.addEventListener(type, func, bubble);
+			});
+
+			return function () {
+				builtListeners.forEach(({ type, func, bubble = false }) => {
+					target.removeEventListener(type, func, bubble);
+				});
+			} as EventListenerRemover;
 		};
 	};
 }
