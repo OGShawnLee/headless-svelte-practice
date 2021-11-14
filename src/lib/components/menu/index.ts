217bf6c28ff06f82ef30e6325d87fba83a9252a9
Shawn Lee <changoloco.evg@gmail.com>
2021-11-14 09:18:31 -0600

    export menu components

---
+++ src/lib/components/menu/index.ts
@@ -0,0 +1,6 @@
+import Menu from './Menu.svelte';
+export { default as MenuItem } from './Item.svelte';
+export { default as MenuItems } from './Items.svelte';
+export { default as MenuButton } from './Button.svelte';
+
+export default Menu;
