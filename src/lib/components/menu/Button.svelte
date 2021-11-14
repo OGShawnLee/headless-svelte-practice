c0d009fe4fb832ff8280505858c064c26ea3cb46
Shawn Lee <changoloco.evg@gmail.com>
2021-11-14 09:25:53 -0600

    create menu button component

---
+++ src/lib/components/menu/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { isMenuContext, MENU_CONTEXT_KEY } from './Menu.svelte';
+
+	let className = '';
+	export { className as class };
+
+	const MenuContext = getContext(MENU_CONTEXT_KEY);
+	if (!isMenuContext(MenuContext)) throw Error('Invalid Menu Context');
+
+	const { button } = MenuContext;
+</script>
+
+<button class={className} use:button>
+	<slot />
+</button>
