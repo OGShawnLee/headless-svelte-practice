29e2e08bad42868ce2401e41bb072628ee831e6a
Shawn Lee <changoloco.evg@gmail.com>
2021-11-14 13:11:35 -0600

    add focusable elements selectors

--- src/lib/utils/DOMController.ts
+++ src/lib/utils/DOMController.ts
@@ -13,3 +13,18 @@ export class DOMController {
 		return node.tabIndex >= 0;
 	}
 }
+
+// https://github.com/Duder-onomy/svelte-focus-trap/blob/master/src/utils.js
+const FOCUSABLE_ELEMENTS = [
+	'a[href]',
+	'area[href]',
+	'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+	'select:not([disabled]):not([aria-hidden])',
+	'textarea:not([disabled]):not([aria-hidden])',
+	'button:not([disabled]):not([aria-hidden])',
+	'iframe',
+	'object',
+	'embed',
+	'[contenteditable]',
+	'[tabindex]:not([tabindex^="-"])',
+];
