1e94c6690340139291280a5824065cc29efd7456
Shawn Lee <changoloco.evg@gmail.com>
2021-11-14 15:50:03 -0600

    add Menu component definition

---
+++ src/lib/components/menu/Menu.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+	import type { Readable } from 'svelte/store';
+	import type { SelectedStyles, Toggleable } from '$lib/types';
+	import { propsIn } from '$lib/utils/predicate';
+
+	export const isMenuContext = (val: unknown): val is MenuContext =>
+		propsIn(val, 'Open', 'close', 'menu', 'item', 'button');
+
+	interface MenuContext extends Pick<Toggleable, 'close'> {
+		Open: Readable<boolean>;
+		items: (node: HTMLElement, styles?: SelectedStyles) => void;
+		item: (node: HTMLElement) => void;
+		button: (node: HTMLElement) => void;
+	}
+
+	export const MENU_CONTEXT_KEY = 'svelte-headless-menu';
+</script>
