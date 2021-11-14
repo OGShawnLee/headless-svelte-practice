11cc6add65ebcaa58c2e481539aeb06632c6320f
Shawn Lee <changoloco.evg@gmail.com>
2021-11-14 09:24:52 -0600

    create menu items component

---
+++ src/lib/components/menu/Items.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { isMenuContext, MENU_CONTEXT_KEY } from './Menu.svelte';
+
+	let className = '';
+	export { className as class };
+
+	const MenuContext = getContext(MENU_CONTEXT_KEY);
+	if (!isMenuContext(MenuContext)) throw Error('Invalid Menu Context');
+
+	const { Open, menu } = MenuContext;
+</script>
+
+{#if $Open}
+	<div class={className} use:menu>
+		<slot />
+	</div>
+{/if}
