bd6bb22a2cf83e406dd11552f158531d7b841db1
Shawn Lee <changoloco.evg@gmail.com>
2021-11-15 07:21:25 -0600

    export disclosure components

---
+++ src/lib/components/disclosure/index.ts
@@ -0,0 +1,5 @@
+import Disclosure from './Disclosure.svelte';
+export { default as DisclosureButton } from './Button.svelte';
+export { default as DisclosurePanel } from './Panel.svelte';
+
+export default Disclosure;
