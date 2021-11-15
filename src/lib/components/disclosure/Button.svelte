ef466bf45bba3f1ca676f0e62555fdbfc022c40b
Shawn Lee <changoloco.evg@gmail.com>
2021-11-15 07:42:18 -0600

    create disclosure button component

---
+++ src/lib/components/disclosure/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { DISCLOSURE_CONTEXT_KEY, isDisclosureContext } from './Disclosure.svelte';
+
+	let className = '';
+	export { className as class };
+
+	const DisclosureContext = getContext(DISCLOSURE_CONTEXT_KEY);
+	if (!isDisclosureContext(DisclosureContext))
+		throw new Error('Invalid Disclosure Context');
+
+	const { button } = DisclosureContext;
+</script>
+
+<button use:button type="button" class={className}>
+	<slot />
+</button>
