82c53ff8927bd5ba66a1e0b0518a80f708af34a8
Shawn Lee <changoloco.evg@gmail.com>
2021-11-15 22:25:54 -0600

    export dialog components

---
+++ src/lib/components/dialog/index.ts
@@ -0,0 +1,7 @@
+import Dialog from './Dialog.svelte';
+export { default as DialogModal } from './Modal.svelte';
+export { default as DialogOverlay } from './Overlay.svelte';
+export { default as DialogTitle } from './Title.svelte';
+export { default as DialogDescription } from './Description.svelte';
+
+export default Dialog;
