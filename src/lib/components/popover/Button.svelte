3846774e321cb934dcd5055486026040b558d6d5
Shawn Lee <changoloco.evg@gmail.com>
2021-11-16 16:49:15 -0600

    create popover button component

---
+++ src/lib/components/popover/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { isPopoverContext, POPOVERS_CONTEXT_KEY } from './Popover.svelte';
+
+	let className = '';
+	export { className as class };
+
+	const PopoverContext = getContext(POPOVERS_CONTEXT_KEY);
+	if (!isPopoverContext(PopoverContext)) throw new Error('Invalid Popover Context');
+
+	const { button } = PopoverContext;
+</script>
+
+<button class={className} use:button>
+	<slot />
+</button>
