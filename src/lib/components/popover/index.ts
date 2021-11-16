d45ffd524ffae940eab32362bda241278b4ca2fc
Shawn Lee <changoloco.evg@gmail.com>
2021-11-16 16:48:19 -0600

    export popover components

---
+++ src/lib/components/popover/index.ts
@@ -0,0 +1,6 @@
+import Popover from './Popover.svelte';
+export { default as PopoverGroup } from './Group.svelte';
+export { default as PopoverPanel } from './Panel.svelte';
+export { default as PopoverButton } from './Button.svelte';
+
+export default Popover;
