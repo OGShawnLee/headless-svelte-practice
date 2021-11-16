a553c9c2c8e6db6d0f31e9fa4bb12f7977b9d0f9
Shawn Lee <changoloco.evg@gmail.com>
2021-11-16 16:49:37 -0600

    create popover panel component

---
+++ src/lib/components/popover/Panel.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { isPopoverContext, POPOVERS_CONTEXT_KEY } from './Popover.svelte';
+
+	let className = '';
+	export { className as class };
+
+	const PopoverContext = getContext(POPOVERS_CONTEXT_KEY);
+	if (!isPopoverContext(PopoverContext)) throw new Error('Invalid Popover Context');
+
+	const { Open, panel } = PopoverContext;
+</script>
+
+{#if $Open}
+	<div class={className} use:panel>
+		<slot />
+	</div>
+{/if}
