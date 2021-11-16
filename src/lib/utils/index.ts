fa59bf67f35ae2fd07f2acd73c74aea508d21859
Shawn Lee <changoloco.evg@gmail.com>
2021-11-15 22:25:32 -0600

    export DOMController class

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,3 @@
 export { useSubscribers } from './useSubscribers';
 export { handleSelectedStyles } from './handleSelectedStyles';
-export { FocusManager } from './focus-management';
+export { DOMController, FocusManager } from './focus-management';
