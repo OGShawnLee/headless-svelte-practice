c62fe0ccc663d9903da0c524fbf4807169bf9173
Shawn Lee <changoloco.evg@gmail.com>
2021-11-17 16:46:23 -0600

    export staticHash hashable and selectable stores

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -2,3 +2,6 @@ export { navigable } from './navigable';
 export { registrable } from './registrable';
 export { notifiable } from './notifiable';
 export { toggleable } from './toggleable';
+export { staticHash } from './staticHash';
+export { hashable } from './hashable';
+export { selectable } from './selectable';
