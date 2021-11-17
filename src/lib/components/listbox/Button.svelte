b6911cc1761e9eb34687dc9bff573cf04aa5516e
Shawn Lee <changoloco.evg@gmail.com>
2021-11-17 16:47:53 -0600

    create listbox Button component

---
+++ src/lib/components/listbox/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { getContext } from 'svelte';
+	import { isListboxContext, LISTBOX_CONTEXT_KEY } from './Listbox.svelte';
+
+	const ListboxContext = getContext(LISTBOX_CONTEXT_KEY);
+	if (!isListboxContext(ListboxContext)) throw Error('Invalid Listbox Context');
+
+	let className = '';
+	export { className as class };
+
+	const { button } = ListboxContext;
+</script>
+
+<button class={className} use:button>
+	<slot />
+</button>
