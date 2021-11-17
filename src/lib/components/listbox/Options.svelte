42d4fff4b342e8878a28481c0655a9b014949e32
Shawn Lee <changoloco.evg@gmail.com>
2021-11-17 16:49:39 -0600

    create listbox Options component

---
+++ src/lib/components/listbox/Options.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { getContext } from 'svelte';
+	import { isListboxContext, LISTBOX_CONTEXT_KEY } from './Listbox.svelte';
+
+	const ListboxContext = getContext(LISTBOX_CONTEXT_KEY);
+	if (!isListboxContext(ListboxContext)) throw Error('Invalid Listbox Context');
+
+	let className = '';
+	export { className as class };
+
+	const { Open, options } = ListboxContext;
+</script>
+
+{#if $Open}
+	<div class={className} use:options>
+		<slot />
+	</div>
+{/if}
