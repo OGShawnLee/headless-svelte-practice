974ae701b54738aee3b8e9cb04e49aaba8a9ae60
Shawn Lee <changoloco.evg@gmail.com>
2021-11-18 08:43:45 -0600

    add radio-group Option component basic definition

---
+++ src/lib/components/radio-group/Option.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+	import type { Writable } from 'svelte/store';
+	import { propsIn } from '$lib/utils/predicate';
+
+	export const RADIO_GROUP_OPTION_CONTEXT_KEY = 'svelte-headless-radio-group-option';
+
+	export interface RadioGroupOptionContext {
+		isUsingLabel: Writable<boolean>;
+		isUsingDescription: Writable<boolean>;
+		OptionName: Writable<string>;
+		OptionLabelName: Writable<string>;
+		OptionDescriptionName: Writable<string>;
+	}
+
+	const keys = ['OptionName', 'OptionLabelName', 'OptionDescriptionName'];
+	export const isRadioOptionContext = (val: unknown): val is RadioGroupOptionContext =>
+		propsIn(val, 'isUsingLabel', 'isUsingDescription', ...keys);
+</script>
