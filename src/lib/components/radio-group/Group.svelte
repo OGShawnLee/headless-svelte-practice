e5f4c45ee7e56d5068cdb18d86d214092f8b1be4
Shawn Lee <changoloco.evg@gmail.com>
2021-11-18 08:41:13 -0600

    create radio-group Group component

--- src/lib/components/radio-group/Group.svelte
+++ src/lib/components/radio-group/Group.svelte
@@ -1,10 +1,11 @@
 <script context="module" lang="ts">
 	import type { Readable, Writable } from 'svelte/store';
 	import type { Selectable, SelectableOption, SelectedStyles } from '$lib/types';
-	import { navigable } from '$lib/stores';
+	import { navigable, registrable, selectable } from '$lib/stores';
 	import { FocusManager, handleSelectedStyles, useSubscribers } from '$lib/utils';
 	import { propsIn } from '$lib/utils/predicate';
 
+	const RADIO_GROUPS = registrable<number>([]);
 	export const RADIO_GROUP_CONTEXT_KEY = 'svelte-headless-radio-group';
 
 	function initRadioGroup<T>({ Selectable, Vertical }: RadioGroupSettings<T>) {
@@ -83,3 +84,52 @@
 		Vertical: Readable<boolean>;
 	}
 </script>
+
+<script lang="ts">
+	import { onDestroy, onMount, setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	let className = '';
+	export { className as class };
+	export let value: any = '';
+	export let horizontal = false;
+
+	const Selectable = selectable(value, (val) => (value = val));
+	const Vertical = writable(!horizontal);
+	const { options, option } = initRadioGroup({ Selectable, Vertical });
+
+	let id: number;
+	onMount(() => (id = RADIO_GROUPS.register()));
+	onDestroy(() => RADIO_GROUPS.unregister(id));
+
+	$: BASENAME = `${RADIO_GROUP_CONTEXT_KEY}-${id}`;
+
+	const GroupName = writable('');
+	const isUsingLabel = writable(false);
+	const isUsingDescription = writable(false);
+
+	$: GroupName.set(BASENAME);
+	$: Selectable.set(value);
+	$: Vertical.set(!horizontal);
+
+	$: groupLabelledby = $isUsingLabel ? `${BASENAME}-label` : undefined;
+	$: groupDescribedby = $isUsingDescription ? `${BASENAME}-description` : undefined;
+
+	setContext(RADIO_GROUP_CONTEXT_KEY, {
+		initRadioOption: Selectable.initOption,
+		option,
+		GroupName,
+		isUsingLabel,
+		isUsingDescription,
+	});
+</script>
+
+<div
+	id={$GroupName}
+	aria-labelledby={groupLabelledby}
+	aria-describedby={groupDescribedby}
+	class={className}
+	use:options
+>
+	<slot {options} {value} />
+</div>
