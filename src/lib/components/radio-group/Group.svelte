18429602b15c4363c1d5fb54c53e3e43cac38936
Shawn Lee <changoloco.evg@gmail.com>
2021-11-18 08:37:40 -0600

    add options component action

--- src/lib/components/radio-group/Group.svelte
+++ src/lib/components/radio-group/Group.svelte
@@ -1,9 +1,46 @@
 <script context="module" lang="ts">
 	import type { Readable, Writable } from 'svelte/store';
-	import type { Selectable, SelectableOption } from '$lib/types';
+	import type { Selectable, SelectableOption, SelectedStyles } from '$lib/types';
+	import { navigable } from '$lib/stores';
+	import { FocusManager, handleSelectedStyles, useSubscribers } from '$lib/utils';
 	import { propsIn } from '$lib/utils/predicate';
 
 	export const RADIO_GROUP_CONTEXT_KEY = 'svelte-headless-radio-group';
+
+	function initRadioGroup<T>({ Selectable, Vertical }: RadioGroupSettings<T>) {
+		const { Keys: Buttons, SelectedIndex: Index } = Selectable;
+		const { watchers, handlers } = navigable({ Items: Buttons, Index, Vertical });
+
+		return {
+			options: (node: HTMLElement, selectedStyles?: SelectedStyles) => {
+				const { handleKeyboard } = handlers;
+				const { watchNavigation, watchSelected } = watchers;
+
+				const stylesHandler = handleSelectedStyles(selectedStyles);
+				const stopSubcribers = useSubscribers(
+					Selectable.listenNewItem(([newItem]) => {
+						stylesHandler({ unselected: newItem });
+					}),
+					Selectable.listenSelection(),
+					watchNavigation(),
+					watchSelected((selected, previous) => {
+						FocusManager.makeFocusable(selected);
+						stylesHandler({ selected, unselected: previous });
+						if (previous) FocusManager.removeFocusable(previous);
+					})
+				);
+
+				node.setAttribute('role', 'radiogroup');
+				node.addEventListener('keydown', handleKeyboard);
+				return {
+					destroy: () => {
+						stopSubcribers();
+						node.removeEventListener('keydown', handleKeyboard);
+					},
+				};
+			},
+		};
+	}
 
 	export const isRadioGroupContext = (val: unknown): val is RadioGroupContext =>
 		propsIn(
