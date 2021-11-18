a2ffdc10ef754f06f17e01ff119500d52f7efffa
Shawn Lee <changoloco.evg@gmail.com>
2021-11-18 08:21:35 -0600

    export radio group components

---
+++ src/lib/components/radio-group/index.ts
@@ -0,0 +1,6 @@
+import RadioGroup from './Group.svelte';
+export { default as RadioGroupOption } from './Option.svelte';
+export { default as RadioGroupDescription } from './Description.svelte';
+export { default as RadioGroupLabel } from './Label.svelte';
+
+export default RadioGroup;
