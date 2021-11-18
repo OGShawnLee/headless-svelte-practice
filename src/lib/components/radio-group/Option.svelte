96fcf33d41e021247ad9a2057b14fb1b3bcfb54b
Shawn Lee <changoloco.evg@gmail.com>
2021-11-18 08:44:45 -0600

    create radio-group Option component

--- src/lib/components/radio-group/Option.svelte
+++ src/lib/components/radio-group/Option.svelte
@@ -16,3 +16,62 @@
 	export const isRadioOptionContext = (val: unknown): val is RadioGroupOptionContext =>
 		propsIn(val, 'isUsingLabel', 'isUsingDescription', ...keys);
 </script>
+
+<script lang="ts">
+	import { getContext, setContext } from 'svelte';
+	import { derived, writable } from 'svelte/store';
+	import { isRadioGroupContext, RADIO_GROUP_CONTEXT_KEY } from './Group.svelte';
+	import { notifiable } from '$lib/stores';
+
+	let className = '';
+	export { className as class };
+	export let value: any = '';
+	export let selected = false;
+
+	let checked = false;
+	const Selected = notifiable(selected, (bool) => (checked = bool));
+
+	const RadioGroupContext = getContext(RADIO_GROUP_CONTEXT_KEY);
+	if (!isRadioGroupContext(RadioGroupContext))
+		throw new Error('Invalid Radio Group Context');
+
+	const { initRadioOption, GroupName, option } = RadioGroupContext;
+	const SelectableOption = initRadioOption({
+		initialValue: value,
+		initialIsSelected: selected,
+		Selected,
+	});
+
+	const Id = SelectableOption.Id;
+	const isUsingLabel = writable(false);
+	const isUsingDescription = writable(false);
+
+	const OptionName = derived([GroupName, Id], ([$GroupName, $Id]) => {
+		return `${$GroupName}-option-${$Id}`;
+	});
+	const OptionLabelName = derived(OptionName, ($OptionName) => `${$OptionName}-label`);
+	const OptionDescriptionName = derived(OptionName, ($OptionName) => {
+		return `${$OptionName}-description`;
+	});
+
+	$: optionLabelledby = $isUsingLabel ? $OptionLabelName : undefined;
+	$: optionDescribedby = $isUsingDescription ? $OptionDescriptionName : undefined;
+
+	setContext(RADIO_GROUP_OPTION_CONTEXT_KEY, {
+		OptionName,
+		OptionLabelName,
+		OptionDescriptionName,
+		isUsingLabel,
+		isUsingDescription,
+	});
+</script>
+
+<div
+	aria-labelledby={optionLabelledby}
+	aria-describedby={optionDescribedby}
+	class={className}
+	id={$OptionName}
+	use:option={SelectableOption}
+>
+	<slot {checked} />
+</div>
