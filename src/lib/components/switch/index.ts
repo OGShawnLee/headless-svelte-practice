008bb2df357b1c30f8452ed0c66ff0fd4e96cd22
Shawn Lee <changoloco.evg@gmail.com>
2021-11-19 09:15:17 -0600

    export switch components

---
+++ src/lib/components/switch/index.ts
@@ -0,0 +1,6 @@
+import Switch from './Switch.svelte';
+export { default as SwitchGroup } from './Group.svelte';
+export { default as SwitchLabel } from './Label.svelte';
+export { default as SwitchDescription } from './Description.svelte';
+
+export default Switch;
