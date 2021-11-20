6d21c0cb80fc18c0a9305a73c2747ebdb851f8e1
Shawn Lee <changoloco.evg@gmail.com>
2021-11-20 07:42:56 -0600

    change createNamer return to a named tuple

--- src/lib/utils/components.ts
+++ src/lib/utils/components.ts
@@ -2,13 +2,13 @@ const MASTER_BASE_NAME = 'svelte-headless';
 
 export function createNamer(componentName: string, id: number) {
 	const baseName = `${MASTER_BASE_NAME}-${componentName}-${id}`;
-	return {
-		baseName,
-		namer: (subcomponentName: string, id?: number) => {
+	return [
+		(subcomponentName: string, id?: number) => {
 			if (id) return `${baseName}-${subcomponentName}-${id}`;
 			return `${baseName}-${subcomponentName}`;
 		},
-	};
+		baseName,
+	] as [namer: (subcomponentName: string, id?: number) => string, baseName: string];
 }
 
 export function* idGenerator() {
