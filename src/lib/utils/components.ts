2d787e5806255f43fe6866d7fe7b78868c3727fa
Shawn Lee <changoloco.evg@gmail.com>
2021-11-20 07:59:46 -0600

    rename createNamer to useNamer and idGenerator to use_id

--- src/lib/utils/components.ts
+++ src/lib/utils/components.ts
@@ -1,6 +1,6 @@
 const MASTER_BASE_NAME = 'svelte-headless';
 
-export function createNamer(componentName: string, id: number) {
+export function useNamer(componentName: string, id: number) {
 	const baseName = `${MASTER_BASE_NAME}-${componentName}-${id}`;
 	return [
 		(subcomponentName: string, id?: number) => {
@@ -11,7 +11,7 @@ export function createNamer(componentName: string, id: number) {
 	] as [namer: (subcomponentName: string, id?: number) => string, baseName: string];
 }
 
-export function* idGenerator() {
+export function* use_id() {
 	let index = 0;
 	while (true) yield index++;
 }
