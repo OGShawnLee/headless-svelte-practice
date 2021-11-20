fb53227aa3a8ab8eb762868243111949720deba8
Shawn Lee <changoloco.evg@gmail.com>
2021-11-20 08:19:48 -0600

    export use_id and useNamer

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,4 @@
 export { useSubscribers } from './useSubscribers';
 export { handleSelectedStyles } from './handleSelectedStyles';
 export { DOMController, FocusManager } from './focus-management';
+export { use_id, useNamer } from './components';
