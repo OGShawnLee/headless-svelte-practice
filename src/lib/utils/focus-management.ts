c3725b61514bf493502e32478835dc6fd221f89d
Shawn Lee <changoloco.evg@gmail.com>
2021-11-20 19:16:37 -0600

    switch object oriented code for functional
    it's all functions

--- src/lib/utils/focus-management.ts
+++ src/lib/utils/focus-management.ts
@@ -1,120 +1,112 @@
 import { isHTMLElement } from './predicate';
 
-export class FocusManager extends DOMController {
-	node: HTMLElement;
-	internalElements: Set<HTMLElement>;
-	externalElements: Set<HTMLElement>;
+export function focusFirstElement(node: HTMLElement) {
+	const internalElements: HTMLElement[] = [];
+	useInternalElements(node, internalElements);
 
-	constructor(node: HTMLElement) {
-		super();
-		this.node = node;
-		this.internalElements = new Set();
-		this.externalElements = new Set();
+	const firstElement = internalElements.find((element) => element.tabIndex >= 0);
+	return firstElement?.focus(), Boolean(firstElement);
+}
 
-		for (let index = 0; index < FOCUSABLE_ELEMENTS.length; index++) {
-			const selector = FOCUSABLE_ELEMENTS[index];
-			const selectedElements = document.querySelectorAll(selector);
+export function isFocusable(node: HTMLElement) {
+	return node.tabIndex >= 0;
+}
 
-			for (let counter = 0; counter < selectedElements.length; counter++) {
-				const element = selectedElements[counter];
+export function makeFocusable(node: HTMLElement, tabIndex = 0) {
+	return (node.tabIndex = tabIndex), node;
+}
 
-				if (!isHTMLElement(element)) continue;
-				if (node.contains(element)) this.internalElements.add(element);
-				else this.externalElements.add(element);
-			}
+export function removeFocusable(node: HTMLElement) {
+	const originalTabIndex = node.tabIndex;
+	node.tabIndex = -1;
+	return () => makeFocusable(node, originalTabIndex);
+}
+
+function useElements(node: HTMLElement) {
+	const internalElements = new Set<HTMLElement>();
+	const externalElements = new Set<HTMLElement>();
+
+	for (let index = 0; index < FOCUSABLE_ELEMENTS.length; index++) {
+		const selector = FOCUSABLE_ELEMENTS[index];
+		const selectedElements = document.querySelectorAll(selector);
+
+		for (let counter = 0; counter < selectedElements.length; counter++) {
+			const element = selectedElements[counter];
+
+			if (!isHTMLElement(element)) continue;
+			if (node.contains(element)) internalElements.add(element);
+			else externalElements.add(element);
 		}
 	}
 
-	private handleBrowserFocus(
-		internalElements: HTMLElement[],
-		fallback?: HTMLElement | null
-	) {
-		const numberOfElements = internalElements.length;
-		const self = this;
+	let arrInternal = Array.from(internalElements);
+	let arrExternal = Array.from(externalElements);
+	return [arrInternal, arrExternal] as [internal: HTMLElement[], external: HTMLElement[]];
+}
 
-		function handleFocus(event: KeyboardEvent, target?: HTMLElement | null) {
-			event.preventDefault(), target?.focus();
+function useInternalElements(node: HTMLElement, arr: HTMLElement[]) {
+	if (!node.hasChildNodes()) return arr.push(node), void 0;
+	Array.from(node.children).forEach((child) => {
+		if (isHTMLElement(child)) {
+			arr.push(child), useInternalElements(child, arr);
 		}
+	});
+}
 
-		function handleTrapFocus(event: KeyboardEvent) {
-			const { key, shiftKey } = event;
-			const activeElement = document.activeElement;
+function usePreventBrowserFocus(
+	node: HTMLElement,
+	internalElements: HTMLElement[],
+	fallback?: HTMLElement | Element | null
+) {
+	const numberOfElements = internalElements.length;
 
-			const firstElement = internalElements.find((element) => element.tabIndex >= 0);
-			const lastElement = internalElements.at(-1);
-
-			if (key === 'Tab') {
-				if (numberOfElements === 0) return handleFocus(event, fallback);
-				if (numberOfElements === 1) return handleFocus(event, firstElement);
-
-				if (self.node === activeElement) return event.preventDefault();
-				if (activeElement === firstElement) {
-					if (shiftKey) handleFocus(event, lastElement);
-				} else if (activeElement === lastElement) {
-					if (!shiftKey) handleFocus(event, firstElement);
-				}
-			}
-		}
-
-		window.addEventListener('keydown', handleTrapFocus);
-		return function () {
-			window.removeEventListener('keydown', handleTrapFocus);
-		};
+	function handleFocus(event: KeyboardEvent, target?: HTMLElement | Element | null) {
+		event.preventDefault();
+		if (isHTMLElement(target)) target.focus();
 	}
 
-	static getInternalElements(node: HTMLElement, arr: HTMLElement[]) {
-		if (!node.hasChildNodes()) return arr.push(node), void 0;
-		Array.from(node.children).forEach((child) => {
-			if (isHTMLElement(child)) {
-				arr.push(child), this.getInternalElements(child, arr);
-			}
-		});
-	}
-
-	static focusFirstElement(node: HTMLElement) {
-		const internalElements: HTMLElement[] = [];
-		this.getInternalElements(node, internalElements);
+	return function (event: KeyboardEvent) {
+		const { key, shiftKey } = event;
+		const activeElement = document.activeElement;
 
 		const firstElement = internalElements.find((element) => element.tabIndex >= 0);
-		return firstElement?.focus(), Boolean(firstElement);
-	}
+		const lastElement = internalElements.at(-1);
 
-	public trapFocus(fallback?: HTMLElement | null) {
-		const ORIGINAL_INDEXES: number[] = [];
-		this.externalElements.forEach((element) => {
-			ORIGINAL_INDEXES.push(element.tabIndex);
-			element.tabIndex = -1;
+		if (key === 'Tab') {
+			if (numberOfElements === 0) return handleFocus(event, fallback);
+			if (numberOfElements === 1) return handleFocus(event, firstElement);
+
+			if (node === activeElement) return event.preventDefault();
+			if (activeElement === firstElement) {
+				if (shiftKey) handleFocus(event, lastElement);
+			} else if (activeElement === lastElement) {
+				if (!shiftKey) handleFocus(event, firstElement);
+			}
+		}
+	};
+}
+
+export function useFocusTrap(node: HTMLElement, fallback?: HTMLElement | Element | null) {
+	const [internal, external] = useElements(node);
+	const ORIGINAL_INDEXES = external.map((element) => {
+		const originalIndex = element.tabIndex;
+		element.tabIndex = -1;
+		return originalIndex;
+	});
+
+	const preventBrowserFocus = usePreventBrowserFocus(node, internal, fallback);
+	window.addEventListener('keydown', preventBrowserFocus);
+	return () => {
+		window.removeEventListener('keydown', preventBrowserFocus);
+		ORIGINAL_INDEXES.forEach((tabIndex, index) => {
+			const element = external[index];
+			element.tabIndex = tabIndex;
 		});
+	};
+}
 
-		const arrElements = Array.from(this.internalElements.values());
-		const removeBrowserFocus = this.handleBrowserFocus(arrElements, fallback);
-		return () => {
-			Array.from(this.externalElements).forEach((element, index) => {
-				const tabIndex = ORIGINAL_INDEXES[index];
-				element.tabIndex = tabIndex;
-			});
-
-			removeBrowserFocus();
-		};
-	}
-
-	static handleSimpleTrapFocus(event: KeyboardEvent) {
-		if (event.key === 'Tab') event.preventDefault();
-	}
-
-	static makeFocusable(node: HTMLElement, tabIndex = 0) {
-		return (node.tabIndex = tabIndex), node;
-	}
-
-	static removeFocusable(node: HTMLElement) {
-		const originalTabIndex = node.tabIndex;
-		node.tabIndex = -1;
-		return () => this.makeFocusable(node, originalTabIndex);
-	}
-
-	static isFocusable(node: HTMLElement) {
-		return node.tabIndex >= 0;
-	}
+export function useSimpleFocusTrap(event: KeyboardEvent) {
+	if (event.key === 'Tab') event.preventDefault();
 }
 
 // https://github.com/Duder-onomy/svelte-focus-trap/blob/master/src/utils.js
