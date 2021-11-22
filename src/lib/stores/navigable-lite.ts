6a1db245b3a0e495c950109d72de58708db908b9
Shawn Lee <changoloco.evg@gmail.com>
2021-11-22 13:03:03 -0600

    add NavigableLite store
    - Doesn't include support for Waiting and Vertical Waiting
    - Only includes the necessary methods
    - Additional methods will be added as external functions
    - New handler approach; add listener and subscriber, return unsubscriber
    - Uses a new subscription approach; there is not a single get function
    - Should improve performance

---
+++ src/lib/stores/navigable-lite.ts
@@ -0,0 +1,148 @@
+import type { Readable, Writable } from 'svelte/store';
+import type { NavigableLite } from '$lib/types';
+import { derived, readable, writable } from 'svelte/store';
+import { isBoolean } from '$lib/utils/predicate';
+import { useSubscribers } from '$lib/utils';
+import { unstoppable } from '$lib/stores';
+
+export function navigableLite({ items, ...Optional }: Settings): NavigableLite {
+	let {
+		Index = writable(0),
+		Manual = readable(false),
+		Vertical = readable(false),
+	} = Optional;
+
+	if (isBoolean(Manual)) Manual = readable(Manual);
+	if (isBoolean(Vertical)) Vertical = readable(Vertical);
+
+	const ManualIndex = writable(0);
+	const TargetIndex = derived(Manual as Readable<boolean>, ($Manual) => {
+		return $Manual ? ManualIndex : Index;
+	});
+
+	const Selected = derived(Index, ($Index) => items[$Index]);
+
+	function isOverflowed(
+		index: number,
+		direction: 'ASCENDING' | 'DESCENDING',
+		length: number
+	) {
+		if (direction === 'ASCENDING') return index + 1 === length;
+		if (direction === 'DESCENDING') return index - 1 === -1;
+		throw new Error('Invalid Direction');
+	}
+
+	function navigate(
+		direction: 'ASCENDING' | 'DESCENDING',
+		Index: Writable<number>,
+		callback: (index: number, isOverflowed: boolean) => number
+	) {
+		Index.update((index) => {
+			const overflowed = isOverflowed(index, direction, items.length);
+			const newIndex = callback(index, overflowed);
+			return items[newIndex].focus(), newIndex;
+		});
+	}
+
+	function goFirst() {
+		return 0;
+	}
+
+	function goLast() {
+		return items.length - 1;
+	}
+
+	function goPrev(Index: Writable<number>, ctrlKey: boolean) {
+		navigate('DESCENDING', Index, (index, isOverflowed) => {
+			if (ctrlKey) return goFirst();
+			return isOverflowed ? goLast() : index - 1;
+		});
+	}
+
+	function goNext(Index: Writable<number>, ctrlKey: boolean) {
+		navigate('ASCENDING', Index, (index, isOverflowed) => {
+			if (ctrlKey) return goLast();
+			return isOverflowed ? goFirst() : index + 1;
+		});
+	}
+
+	return {
+		Index,
+		ManualIndex,
+		useNavigation(node, callback) {
+			const ARROWS = ['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'];
+			const Arrow = unstoppable('');
+			let should_run = false;
+			let usedCtrl = false;
+
+			function handleKeyboard({ key, ctrlKey }: KeyboardEvent) {
+				usedCtrl = ctrlKey;
+				if (ARROWS.includes(key)) {
+					should_run = true;
+					Arrow.set(key);
+				}
+			}
+
+			const MainLoop = derived(
+				[Arrow, TargetIndex, Vertical as Readable<boolean>],
+				([$Arrow, $Index, $Vertical]) =>
+					[$Arrow, $Index, $Vertical] as [string, Writable<number>, boolean]
+			);
+
+			const STOP_SUBSCRIBERS = useSubscribers(
+				Index.subscribe((index) => {
+					ManualIndex.set(index);
+					if (callback) callback(index);
+				}),
+				MainLoop.subscribe(([arrow, Index, vertical]) => {
+					if (!should_run) return;
+					should_run = false;
+
+					arrow = Arrow.resolve();
+					if (vertical)
+						switch (arrow) {
+							case 'ArrowUp':
+								return goPrev(Index, usedCtrl);
+							case 'ArrowDown':
+								return goNext(Index, usedCtrl);
+						}
+					else
+						switch (arrow) {
+							case 'ArrowLeft':
+								return goPrev(Index, usedCtrl);
+							case 'ArrowRight':
+								return goNext(Index, usedCtrl);
+						}
+				})
+			);
+
+			node.addEventListener('keydown', handleKeyboard);
+			return function () {
+				STOP_SUBSCRIBERS();
+				node.removeEventListener('keydown', handleKeyboard);
+			};
+		},
+		handleSelection: (index) => {
+			return function () {
+				Index.set(index), ManualIndex.set(index);
+			};
+		},
+		listenSelected: (callback) => {
+			let previous: HTMLElement | undefined;
+			return Selected.subscribe((selected) => {
+				if (selected && selected !== previous) callback(selected, previous);
+				previous = selected;
+			});
+		},
+		isSelected: (index, callback) => {
+			return Index.subscribe((Index) => callback(Index === index));
+		},
+	};
+}
+
+interface Settings {
+	items: HTMLElement[];
+	Index?: Writable<number>;
+	Manual?: Readable<boolean> | boolean;
+	Vertical?: Readable<boolean> | boolean;
+}
