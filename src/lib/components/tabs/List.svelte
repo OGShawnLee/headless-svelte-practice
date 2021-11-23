0c284c78a52598b3f93ab50e4ba195f2ede421d9
Shawn Lee <changoloco.evg@gmail.com>
2021-11-22 19:15:03 -0600

    remove SelectedStyles in response to major refactor

--- src/lib/components/tabs/List.svelte
+++ src/lib/components/tabs/List.svelte
@@ -3,17 +3,14 @@
 	import { isTabsContext, TABS_CONTEXT_KEY } from './Group.svelte';
 
 	let className = '';
-
 	export { className as class };
-	export let active = '';
-	export let unactive = '';
 
 	const TabsContext = getContext(TABS_CONTEXT_KEY);
 	if (!isTabsContext(TabsContext)) throw new Error('Invalid Tabs Context');
 
-	const { tabs } = TabsContext;
+	const { tablist } = TabsContext;
 </script>
 
-<div class={className} use:tabs={{ if: active, else: unactive }}>
+<div class={className} use:tablist>
 	<slot />
 </div>
