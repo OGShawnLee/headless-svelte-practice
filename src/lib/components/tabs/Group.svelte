4595bb90045dcf1826fd5cba2adbb44b0547dc22
Shawn Lee <changoloco.evg@gmail.com>
2021-11-22 19:09:04 -0600

    introduce major refactor
    - Add complete Aria support
    - Use NavigableLite, improved performance and methods
    - Remove SelectedStyles feature, will be added later
    - Opdate isTabsContext predicate function

--- src/lib/components/tabs/Group.svelte
+++ src/lib/components/tabs/Group.svelte
@@ -1,71 +1,99 @@
 <script context="module" lang="ts">
 	import type { Readable } from 'svelte/store';
-	import type { Notifiable, Notifier, SelectedStyles } from '$lib/types';
-	import { navigable, notifiable, registrable } from '$lib/stores';
-	import { useSubscribers, handleSelectedStyles } from '$lib/utils';
-	import { isObject } from '$lib/utils/predicate';
+	import type { Notifiable, Notifier } from '$lib/types';
+	import { navigableLite, useManualBlur } from '$lib/stores/navigable-lite';
+	import { hashable, notifiable, staticRegistrable } from '$lib/stores';
+	import { useNamer, useStoreListeners, useSubscribers, use_id } from '$lib/utils';
 	import { makeFocusable, removeFocusable } from '$lib/utils/focus-management';
+	import { propsIn } from '$lib/utils/predicate';
 
 	export const TABS_CONTEXT_KEY = 'SVELTE-HEADLESS-TABS';
+	const generate_id = use_id();
 
 	function initTabs({ Index, Manual, Vertical }: TabsSettings) {
-		const Tabs = registrable<HTMLElement>([]);
-		const Panels = registrable<number>([]);
-		const config = { Items: Tabs, Index, Manual, Vertical };
-		const { handlers, watchers, ...Navigable } = navigable(config);
+		const { value: items, ...Tabs } = staticRegistrable<HTMLElement>([]);
+		const Panels = hashable<number, string | undefined>();
+		const Navigable = navigableLite({ items, Index, Manual, Vertical });
 
+		const id = generate_id.next().value as number;
+		const [baptize, tabsName] = useNamer('tabs', id);
 		return {
-			tabs: (node: HTMLElement, styles?: SelectedStyles) => {
-				const { handleKeyboard, createManualBlurHandler } = handlers;
-				const { watchNavigation, watchSelected } = watchers;
+			tablist: (node: HTMLElement) => {
+				const { Index, ManualIndex } = Navigable;
+				Tabs.useItems((tab) => (tab.ariaSelected = String(false)));
 
-				const stylesHandler = handleSelectedStyles(styles);
-				Tabs.useItems((tab) => stylesHandler({ unselected: tab }));
-
-				const DisposeSubscribers = useSubscribers(
-					Tabs.watchers.watchNewItem((newTab) => {
-						stylesHandler({ unselected: newTab });
-					}),
-					watchNavigation(),
-					watchSelected((selected, previous) => {
-						stylesHandler({ selected, unselected: previous });
-						if (previous) removeFocusable(previous);
+				const STOP_SUBSCRIBERS = useSubscribers(
+					Navigable.listenSelected((selected, previous) => {
 						makeFocusable(selected);
+						selected.ariaSelected = String(true);
+						if (previous) {
+							removeFocusable(previous);
+							previous.ariaSelected = String(false);
+						}
+					}),
+					Vertical.subscribe((isVertical) => {
+						node.ariaOrientation = isVertical ? 'vertical' : 'horizontal';
 					})
 				);
 
-				const { handleManualBlur, removeInternal } = createManualBlurHandler(node);
-				node.addEventListener('keydown', handleKeyboard);
-				node.addEventListener('focusin', handleManualBlur);
+				const STOP_LISTENERS = useStoreListeners(
+					Navigable.useNavigation(node),
+					useManualBlur(node, items, Index, ManualIndex)
+				);
+
+				node.id = tabsName;
+				node.setAttribute('role', 'tablist');
 				return {
 					destroy: () => {
-						DisposeSubscribers;
-						node.removeEventListener('keydown', handleKeyboard);
-						node.removeEventListener('focusin', handleManualBlur);
-						removeInternal();
+						STOP_SUBSCRIBERS(), STOP_LISTENERS();
 					},
 				};
 			},
 			tab: (node: HTMLElement, notifySelected: Notifier<boolean>) => {
-				const registeredIndex = Tabs.register(node, removeFocusable);
+				const index = Tabs.register(node, removeFocusable);
+				const selectTab = Navigable.handleSelection(index);
+				const tabName = baptize('tab', index);
 
-				const StopSelected = watchers.watchIsSelected(registeredIndex, notifySelected);
+				const stopSubscribers = useSubscribers(
+					Navigable.isSelected(index, notifySelected),
+					Panels.listenItem(index, (panelName) => {
+						if (panelName) node.setAttribute('aria-controls', panelName);
+						else node.removeAttribute('aria-controls');
+					})
+				);
 
-				const selectTab = handlers.handleSelection(registeredIndex);
+				node.id = tabName;
+				node.setAttribute('role', 'tab');
 				node.addEventListener('click', selectTab);
 				return {
 					destroy: () => {
-						Tabs.unregister(node), StopSelected();
+						Tabs.unregister(index), stopSubscribers();
 						node.removeEventListener('click', selectTab);
 					},
 				};
 			},
-			panel: {
-				register: Panels.register,
-				unregister: Panels.unregister,
+			usePanel: {
+				register: () => Panels.preRegister(undefined),
+				panel: (node: HTMLElement, index: number) => {
+					const [tabName, panelName] = [baptize('tab', index), baptize('panel', index)];
+					Panels.update(index, panelName);
+
+					node.tabIndex = 0;
+					node.id = panelName;
+					node.setAttribute('role', 'tabpanel');
+					node.setAttribute('aria-labelledby', tabName);
+					return {
+						destroy: () => {
+							Panels.unregister(index);
+						},
+					};
+				},
 			},
 		};
 	}
+
+	export const isTabsContext = (val: unknown): val is TabsContext =>
+		propsIn(val, 'Index', 'tab', 'tablist', 'usePanel');
 
 	interface TabsSettings {
 		Index: Notifiable<number>;
@@ -73,39 +101,33 @@
 		Vertical: Readable<boolean>;
 	}
 
-	export function isTabsContext(val: unknown): val is TabsContext {
-		if (!isObject(val)) return false;
-		if (!('Index' in val)) return false;
-		if (!('panel' in val)) return false;
-		return 'tabs' in val && 'tab' in val;
-	}
-
 	interface TabsContext extends ReturnType<typeof initTabs> {
-		Index: Notifiable<number>;
+		Index: Readable<number>;
 	}
 </script>
 
 <script lang="ts">
-	import { setContext } from 'svelte';
 	import { writable } from 'svelte/store';
+	import { setContext } from 'svelte';
 
-	export let current = 0;
+	export let index = 0;
 	export let manual = false;
 	export let vertical = false;
 	export let onChange: (index: number) => void = () => void 0;
 
-	const Index = notifiable(current, (num) => (current = num));
+	const Index = notifiable(index, (num) => (index = num));
 	const Manual = writable(manual);
 	const Vertical = writable(vertical);
 
-	$: Index.set(current);
-	$: Vertical.set(vertical);
+	$: Index.set(index);
 	$: Manual.set(manual);
+	$: Vertical.set(vertical);
+	$: onChange(index);
 
-	$: if (onChange) onChange(current);
+	const TabsState = initTabs({ Index, Manual, Vertical });
+	setContext(TABS_CONTEXT_KEY, { Index, ...TabsState });
 
-	const { tabs, tab, panel } = initTabs({ Index, Vertical, Manual });
-	setContext(TABS_CONTEXT_KEY, { Index, tabs, tab, panel });
+	const { tablist } = TabsState;
 </script>
 
-<slot {current} {tabs} />
+<slot {index} {tablist} />
