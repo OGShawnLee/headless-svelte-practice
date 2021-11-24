5b95f7e4294fc814d430f36e99393502b189989d
Shawn Lee <changoloco.evg@gmail.com>
2021-11-24 09:54:43 -0600

    introduce major refactor
    - Add Aria support
    - Update component naming (id's)
    - Adjust to toggleable refactor

--- src/lib/components/popover/Popover.svelte
+++ src/lib/components/popover/Popover.svelte
@@ -1,41 +1,59 @@
 <script context="module" lang="ts">
 	import type { Toggleable } from '$lib/types';
 	import type { PopoverGroupContext } from '$lib/components/popover/Group.svelte';
-	import { clickOutside, escapeKey, focusLeave } from '$lib/utils/definedListeners';
 	import { derived, Readable } from 'svelte/store';
+	import { useNamer, useSubscribers, use_id } from '$lib/utils';
+	import { clickOutside, escapeKey, focusLeave } from '$lib/utils/definedListeners';
 	import { propsIn } from '$lib/utils/predicate';
 
-	const POPOVERS = registrable<number>([]);
-	export const POPOVERS_CONTEXT_KEY = 'SVELTE-HEADLESS-POPOVER';
+	const generate_id = use_id();
+	export const POPOVER_CONTEXT_KEY = 'SVELTE-HEADLESS-POPOVER';
 
-	function initPopover({ Toggleable, id, GroupContext }: PopoverSettings) {
-		const { useButton, usePanel } = Toggleable;
+	function initPopover({ Toggleable, GroupContext }: PopoverSettings) {
+		const id = generate_id.next().value as number;
+		const [baptize] = useNamer('popover', id);
 
+		const buttonName = baptize('button');
 		function isolatedButton(node: HTMLElement) {
-			const disposeButton = useButton(node);
+			const Panel = Toggleable.Panel;
+			const DISPOSE_BUTTON = Toggleable.useButton(node);
+
+			const STOP_SUBSCRIBERS = useSubscribers(
+				Toggleable.subscribe((isOpen) => {
+					node.ariaExpanded = String(isOpen);
+				}),
+				Panel.subscribe((panel) => {
+					if (panel) node.setAttribute('aria-controls', panelName);
+					else node.removeAttribute('aria-controls');
+				})
+			);
+
+			node.id = buttonName;
+			node.ariaHasPopup = 'true';
 			return {
-				destroy: () => {
-					disposeButton();
+				destroy() {
+					DISPOSE_BUTTON(), STOP_SUBSCRIBERS();
 				},
 			};
 		}
 
+		const panelName = baptize('panel');
 		function isolatedPanel(node: HTMLElement) {
-			const disposePanel = usePanel({
+			const DISPOSE_PANEL = Toggleable.usePanel({
+				listeners: [escapeKey, clickOutside, focusLeave],
 				panelElement: node,
-				listenersBuilders: [escapeKey, clickOutside, focusLeave],
 			});
 
+			node.id = panelName;
 			return {
-				destroy: () => {
-					disposePanel();
-				},
+				destroy: () => DISPOSE_PANEL(),
 			};
 		}
 
+		const { useGroupButton, useGroupPanel } = GroupContext || {};
 		return {
-			button: GroupContext?.useGroupButton(Toggleable, id) || isolatedButton,
-			panel: GroupContext?.useGroupPanel(Toggleable, id) || isolatedPanel,
+			button: GroupContext ? useGroupButton!(Toggleable, id) : isolatedButton,
+			panel: GroupContext ? useGroupPanel!(Toggleable, id) : isolatedPanel,
 		};
 	}
 
@@ -44,7 +62,6 @@
 
 	interface PopoverSettings {
 		Toggleable: Toggleable;
-		id: number;
 		GroupContext?: PopoverGroupContext;
 	}
 
@@ -54,28 +71,25 @@
 </script>
 
 <script lang="ts">
-	import { registrable, toggleable } from '$lib/stores';
-	import { getContext, onDestroy, setContext } from 'svelte';
+	import { toggleable } from '$lib/stores';
+	import { getContext, setContext } from 'svelte';
 	import { isPopoverGroupContext, POPOVER_GROUP_CONTEXT_KEY } from './Group.svelte';
 
-	const id = POPOVERS.register();
-	onDestroy(() => POPOVERS.unregister(id));
-
 	let open = false;
+
 	let className = '';
-	export { className as closeAll };
+	export { className as class };
 
 	const GroupContext = getContext(POPOVER_GROUP_CONTEXT_KEY);
-	if (GroupContext !== undefined) {
-		if (!isPopoverGroupContext(GroupContext))
-			throw new Error('Invalid Popover Group Context');
-	}
+	if (GroupContext !== undefined && !isPopoverGroupContext(GroupContext))
+		throw new Error('Invalid Group Context');
 
 	const Toggleable = toggleable(open, (bool) => (open = bool));
-	const { button, panel } = initPopover({ Toggleable, id, GroupContext });
+	const { button, panel } = initPopover({ Toggleable, GroupContext });
 
 	const Open = derived(Toggleable, ($Open) => $Open);
-	setContext(POPOVERS_CONTEXT_KEY, { Open, button, panel });
+
+	setContext(POPOVER_CONTEXT_KEY, { Open, button, panel });
 </script>
 
 <div class={className}>
