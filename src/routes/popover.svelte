56644896d92188f991b9cf3754556d29780df5a0
Shawn Lee <changoloco.evg@gmail.com>
2021-11-24 11:46:11 -0600

    simplify testing and test Popover refactor

--- src/routes/popover.svelte
+++ src/routes/popover.svelte
@@ -1,74 +1,106 @@
-<script>
+<script lang="ts">
 	import Popover, {
 		PopoverGroup,
 		PopoverButton,
 		PopoverPanel,
 	} from '$lib/components/popover';
-	import { fade } from 'svelte/transition';
+
+	let ref: HTMLElement;
+	let [expanded, toggleExpanded] = [false, () => (expanded = !expanded)];
 </script>
 
-<h1>Popover</h1>
-<h2>Individual Popover</h2>
-<p>
-	Button automatically toggles the popover. Pressing Escape, clicking away or losing focus
-	will close the popover. Upon closing focus returns to the the popover button
-</p>
+<button bind:this={ref}>External</button>
+
 <Popover let:open let:button let:panel let:close>
-	{open}
-	<button use:button> Toggle Popover </button>
+	<button use:button>Toggler {open}</button>
 	<div slot="panel" use:panel>
-		<p>What a wonderful day</p>
+		This is an amazing popover!
 		<button on:click={close}>Close this</button>
 	</div>
 </Popover>
+<Popover let:open let:button let:panel let:close>
+	<button use:button>Toggler {open}</button>
+	<div slot="panel" use:panel>
+		This is an amazing popover!
+		<button on:click={() => close(ref)}>Use Ref</button>
+	</div>
+</Popover>
 
-<h2>Popover Group</h2>
-<p>
-	Clicking or focusing elements inside the group wont close any Popover inside the group.
-	Clicking away, focusing out of the group or pressing Escape will close all popovers.
-	Since there are many Popover Buttons, upon closing the focus stays wherever it was.
-</p>
-<PopoverGroup>
+<br />
+
+<svelte:window on:keydown={({ key }) => key === 't' && toggleExpanded()} />
+
+<button class:active={expanded} on:click={toggleExpanded}>Toggle Expanded</button>
+<PopoverGroup {expanded}>
 	<Popover let:open let:button let:panel let:close>
-		{open}
-		<button use:button> Toggle Popover </button>
+		<button use:button>Toggler {open}</button>
 		<div slot="panel" use:panel>
-			<p>What a wonderful day</p>
+			This is an amazing popover!
 			<button on:click={close}>Close this</button>
 		</div>
 	</Popover>
-
 	<Popover let:open let:button let:panel let:close>
-		{open}
-		<button use:button> Toggle Popover </button>
+		<button use:button>Toggler {open}</button>
 		<div slot="panel" use:panel>
-			<p>What a wonderful day</p>
-			<button on:click={close}>Close this</button>
+			This is an amazing popover!
+			<button on:click={() => close(ref)}>Use Ref</button>
 		</div>
 	</Popover>
-
 	<Popover let:open let:button let:panel let:close>
-		{open}
-		<button use:button> Toggle Popover </button>
+		<button use:button>Toggler {open}</button>
 		<div slot="panel" use:panel>
-			<p>What a wonderful day</p>
-			<button on:click={close}>Close this</button>
+			This is an amazing popover!
+			<button on:click={() => close(ref)}>Use Ref</button>
 		</div>
 	</Popover>
 </PopoverGroup>
 
-<h2>Compound Popover</h2>
-<p>
-	Popover creted with Popover subcomponent. Drawback is that the panel can't have a
-	transition directly on it. User has to wrap the inner content within a normal DOM
-	element with a transition directive
-</p>
-<Popover let:close>
-	<PopoverButton>Toggle</PopoverButton>
+<br />
+
+<Popover let:close let:open>
+	<PopoverButton>Toggle {open}</PopoverButton>
 	<PopoverPanel>
-		<div transition:fade>
-			This is a beautiful popover
-			<button on:click={close}>Close popover</button>
-		</div>
+		This is an amazing popover!
+		<button on:click={close}>Close this</button>
 	</PopoverPanel>
 </Popover>
+
+<Popover let:close let:open>
+	<PopoverButton>Toggle {open}</PopoverButton>
+	<PopoverPanel>
+		This is an amazing popover!
+		<button on:click={() => close(ref)}>Use ref</button>
+	</PopoverPanel>
+</Popover>
+
+<br />
+
+<PopoverGroup {expanded}>
+	<Popover let:open let:close>
+		<PopoverButton>Toggler {open}</PopoverButton>
+		<PopoverPanel>
+			This is an amazing popover!
+			<button on:click={close}>Close this</button>
+		</PopoverPanel>
+	</Popover>
+	<Popover let:open let:close>
+		<PopoverButton>Toggler {open}</PopoverButton>
+		<PopoverPanel>
+			This is an amazing popover!
+			<button on:click={() => close(ref)}>Use Ref</button>
+		</PopoverPanel>
+	</Popover>
+	<Popover let:open let:close>
+		<PopoverButton>Toggler {open}</PopoverButton>
+		<PopoverPanel>
+			This is an amazing popover!
+			<button on:click={() => close(ref)}>Use Ref</button>
+		</PopoverPanel>
+	</Popover>
+</PopoverGroup>
+
+<style>
+	.active {
+		color: green;
+	}
+</style>
