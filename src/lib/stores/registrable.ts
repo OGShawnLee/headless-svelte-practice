7d085219b0e2c990a85ce2882e2b977bbe744a4d
Shawn Lee <changoloco.evg@gmail.com>
2021-12-01 17:45:31 -0600

    Remove staticRegistrable store

--- src/lib/stores/registrable.ts
+++ src/lib/stores/registrable.ts
@@ -58,38 +58,3 @@ export function registrable<T>(val: T[] = []): Registrable<T> {
 		},
 	};
 }
-
-export function staticRegistrable<T>(items: T[]) {
-	const NewItem = writable<T | null>(null);
-
-	return {
-		value: items,
-		register: (val?: T, onRegister?: (val: T) => void) => {
-			val && NewItem.set(val);
-			if (val) {
-				if (items.includes(val)) throw new Error('Duplicate Value');
-
-				onRegister && onRegister(val);
-				return items.push(val) - 1;
-			} else if (isNumberArray(items)) {
-				onRegister && onRegister(val as T);
-				return items.push(items.length) - 1;
-			} else throw new Error('Invalid Value');
-		},
-		extend: () => {},
-		unregister: (registeredIndex: number) => {
-			items.splice(registeredIndex);
-		},
-		useItems: (callback: (val: T) => void) => {
-			for (let index = 0; index < items.length; index++) {
-				const element = items[index];
-				callback(element);
-			}
-		},
-		listenNewItem: (callback: (val: T) => void) => {
-			return NewItem.subscribe((val) => {
-				if (val) callback(val);
-			});
-		},
-	};
-}
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,16 @@ export { useSubscribers } from './useSubscribers';
 export { handleSelectedStyles } from './handleSelectedStyles';
 export { use_id, useNamer } from './components';
 export { useStoreListeners } from './useStoreListeners';
+
+export const KEYS = {
+	ArrowUp: 'ArrowUp',
+	ArrowDown: 'ArrowDown',
+	ArrowRight: 'ArrowRight',
+	ArrowLeft: 'ArrowLeft',
+	Enter: 'Enter',
+	Space: 'Space',
+};
+
+export function isNotValidKey(key: string) {
+	return !(key in KEYS);
+}
