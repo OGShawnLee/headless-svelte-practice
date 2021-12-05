85da7f7456765177fe5fba8ba3d4d71ef71e5c00
Shawn Lee <changoloco.evg@gmail.com>
2021-12-05 16:49:37 -0600

    Add isOverflowed function

--- src/lib/utils/dom-management.ts
+++ src/lib/utils/dom-management.ts
@@ -9,3 +9,13 @@ export function hideScrollbar() {
 export function isWithin(node?: HTMLElement, target?: HTMLElement) {
 	return node?.contains(target || document.activeElement);
 }
+
+export function isOverflowed(
+	index: number,
+	direction: 'ASCENDING' | 'DESCENDING',
+	items: HTMLElement[]
+) {
+	if (direction === 'ASCENDING') return index + 1 === items.length;
+	if (direction === 'DESCENDING') return index - 1 === -1;
+	throw new Error('Invalid Direction');
+}
