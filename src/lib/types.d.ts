b656eb1d405abe29679ac8686b6e5f281b72cd85
Shawn Lee <changoloco.evg@gmail.com>
2021-12-05 16:37:07 -0600

    Update Component interface

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -8,9 +8,11 @@ export interface Activable extends Readable<boolean> {
 	toggle: () => void;
 }
 
-export interface Component {
-	subscribe: (callback: (exists: boolean, id: string) => void) => Unsubscriber;
-	appear: (node: HTMLElement, id: string) => void;
+interface Component<E extends Element = HTMLElement> {
+	node?: E;
+	subscribe: (callback: (node?: E, id?: string) => void) => Unsubscriber;
+	exists: () => [boolean, E | undefined];
+	appear: (node: E, id: string) => void;
 	disappear: () => void;
 }
 
