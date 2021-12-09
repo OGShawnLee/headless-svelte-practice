8aa411f334e8111f3f99a0b8e86078b4f004919f
Shawn Lee <changoloco.evg@gmail.com>
2021-12-09 17:36:16 -0600

    Merge usePreventBrowserFocus into useFocusTrap and drop removing tabindex from each external DOM element

--- src/lib/utils/focus-management.ts
+++ src/lib/utils/focus-management.ts
@@ -55,55 +55,39 @@ function useDOMTraversal(node: HTMLElement) {
 	return useTraversal(node), items;
 }
 
-function usePreventBrowserFocus(
-	node: HTMLElement,
-	internalElements: HTMLElement[],
-	fallback?: HTMLElement | Element | null
-) {
-	const numberOfElements = internalElements.length;
+export function useFocusTrap(node: HTMLElement, fallback?: HTMLElement | Element | null) {
+	const items = useDOMTraversal(node).filter(isFocusable);
+	const numberOfElements = items.length;
 
-	function handleFocus(event: KeyboardEvent, target?: HTMLElement | Element | null) {
+	fallback = document.activeElement;
+
+	function handleKeyboard(event: KeyboardEvent) {
+		const { code, shiftKey } = event;
+		if (code !== 'Tab') return;
+
+		const activeElement = document.activeElement;
+		const firstElement = items[0];
+		const lastElement = items.at(-1);
+
+		if (numberOfElements === 0) return handleFocus(event, fallback);
+		if (numberOfElements === 1) return event.preventDefault();
+
+		if (node === activeElement) return event.preventDefault();
+		if (activeElement === firstElement) {
+			if (shiftKey) handleFocus(event, lastElement);
+		} else if (activeElement === lastElement) {
+			if (!shiftKey) handleFocus(event, firstElement);
+		}
+	}
+
+	function handleFocus(event: Event, target?: HTMLElement | Element | null) {
 		event.preventDefault();
 		if (isHTMLElement(target)) target.focus();
 	}
 
-	return function (event: KeyboardEvent) {
-		const { key, shiftKey } = event;
-		const activeElement = document.activeElement;
-
-		const firstElement = internalElements.find((element) => element.tabIndex >= 0);
-		const lastElement = internalElements.at(-1);
-
-		if (key === 'Tab') {
-			if (numberOfElements === 0) return handleFocus(event, fallback);
-			if (numberOfElements === 1) return handleFocus(event, firstElement);
-
-			if (node === activeElement) return event.preventDefault();
-			if (activeElement === firstElement) {
-				if (shiftKey) handleFocus(event, lastElement);
-			} else if (activeElement === lastElement) {
-				if (!shiftKey) handleFocus(event, firstElement);
-			}
-		}
-	};
-}
-
-export function useFocusTrap(node: HTMLElement, fallback?: HTMLElement | Element | null) {
-	const [internal, external] = useElements(node);
-	const ORIGINAL_INDEXES = external.map((element) => {
-		const originalIndex = element.tabIndex;
-		element.tabIndex = -1;
-		return originalIndex;
-	});
-
-	const preventBrowserFocus = usePreventBrowserFocus(node, internal, fallback);
-	window.addEventListener('keydown', preventBrowserFocus);
-	return () => {
-		window.removeEventListener('keydown', preventBrowserFocus);
-		ORIGINAL_INDEXES.forEach((tabIndex, index) => {
-			const element = external[index];
-			element.tabIndex = tabIndex;
-		});
+	window.addEventListener('keydown', handleKeyboard);
+	return function () {
+		window.removeEventListener('keydown', handleKeyboard);
 	};
 }
 
