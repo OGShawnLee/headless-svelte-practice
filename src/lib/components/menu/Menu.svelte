d9b4ac3cd38a83a18e1c749d4d609740e852b319
Shawn Lee <changoloco.evg@gmail.com>
2021-12-09 14:57:30 -0600

    Rewrite and update component
    - Update to latest navigable and toggleable changes
    - Add Aria support
    - Remove styling from the menu action as it will remain in development
    - Fix several bugs and increase performance

--- src/lib/components/menu/Menu.svelte
+++ src/lib/components/menu/Menu.svelte
@@ -1,86 +1,113 @@
 <script context="module" lang="ts">
 	import type { Readable } from 'svelte/store';
-	import type { Notifier, SelectedStyles, Toggleable } from '$lib/types';
-	import { propsIn } from '$lib/utils/predicate';
-	import { navigable, registrable, toggleable } from '$lib/stores';
-	import { handleSelectedStyles, useSubscribers } from '$lib/utils';
-	import { escapeKey, clickOutside } from '$lib/utils/definedListeners';
+	import type { Keys, NavigableStartWith, Notifier, Toggleable } from '$lib/types';
+	import { isNotValidKey, useNamer, useSubscribers, use_id, propsIn } from '$lib/utils';
+	import { navigable, useKeyMatch, useHoverSync } from '$lib/stores/navigable';
+	import { registrable, toggleable } from '$lib/stores';
+	import {
+		makeFocusable,
+		preventTabbing,
+		removeFocusable,
+	} from '$lib/utils/focus-management';
 
 	export const MENU_CONTEXT_KEY = 'SVELTE-HEADLESS-MENU';
+	const generateId = use_id();
 
-	function initMenu({ Toggleable }: MenuSetttings) {
-		const { close, useButton, usePanel } = Toggleable;
-		const Items = registrable<HTMLElement>([]);
-		const { handlers, watchers, ...Navigable } = navigable({
-			Items,
-			Vertical: true,
-			Wait: true,
-			VerticalWait: true,
-		});
+	function initMenu({ Toggleable }: MenuSettings) {
+		const Items = registrable<HTMLElement>();
+		const Navigable = navigable({ Items, Vertical: true, Wait: true });
 
-		let button: HTMLElement;
+		const id = generateId.next().value as number;
+		const [baptize, menuName] = useNamer('menu', id);
+		const buttonName = baptize('button');
+		const generateItemId = use_id();
+
+		let startWith: NavigableStartWith = 'AUTO';
 		return {
-			menu: (node: HTMLElement, styles?: SelectedStyles) => {
-				const { handleKeyboard, handleKeyMatch } = handlers;
-				const { watchNavigation, watchSelected } = watchers;
-
-				makeFocusable(node).focus();
-				const RestoreFocus = useFocusTrap(node);
-
-				const DisposePanel = usePanel({
-					panelElement: node,
-					beforeClose: RestoreFocus,
-					listenersBuilders: [escapeKey, clickOutside],
-				});
-
-				const stylesHandler = handleSelectedStyles(styles);
-				Items.useItems((item) => stylesHandler({ unselected: item }));
-				const DisposeSubscribers = useSubscribers(
-					Items.watchers.watchNewItem((item) => stylesHandler({ unselected: item })),
-					watchNavigation(),
-					watchSelected((selected, previous) => {
-						stylesHandler({ selected, unselected: previous });
+			button(node: HTMLElement) {
+				const STOP_SUBSCRIBERS = useSubscribers(
+					Toggleable.useButton(node, {
+						useDefaultKeys: false,
+						keysReducer(event, { open, toggle }) {
+							if (isNotValidKey(event.code)) return;
+							switch (event.code as Keys) {
+								case 'Enter':
+								case 'Space':
+									return (startWith = 'FIRST'), toggle(event);
+								case 'ArrowUp':
+									return (startWith = 'LAST'), open();
+								case 'ArrowDown':
+									return (startWith = 'FIRST'), open();
+							}
+						},
+					}),
+					Toggleable.subscribe((isOpen) => {
+						node.ariaExpanded = String(isOpen);
+						if (isOpen) node.setAttribute('aria-controls', menuName);
+						else node.removeAttribute('aria-controls');
 					})
 				);
 
-				window.addEventListener('keydown', handleKeyboard);
-				window.addEventListener('keydown', handleKeyMatch);
+				node.id = buttonName;
+				node.ariaHasPopup = 'true';
 				return {
-					destroy: () => {
-						DisposePanel(), DisposeSubscribers(), RestoreFocus();
-						window.removeEventListener('keydown', handleKeyboard);
-						window.removeEventListener('keydown', handleKeyMatch);
-						Navigable.onDestroy(({ Index, ManualIndex, Waiting, VerticalWaiting }) => {
-							Index.set(0), ManualIndex.set(0);
-							Waiting.set(true), VerticalWaiting.set(true);
-						});
+					destroy() {
+						STOP_SUBSCRIBERS();
 					},
 				};
 			},
-			item: (node: HTMLElement, notifySelected: Notifier<boolean>) => {
-				const registeredIndex = Items.register(node, removeFocusable);
+			menu(node: HTMLElement) {
+				makeFocusable(node);
+				Navigable.useDynamicOpen(node, ({ isWaiting }) => {
+					return isWaiting ? startWith : 'AUTO';
+				});
 
-				const StopIsSelected = useSubscribers(
-					watchers.watchIsSelected(registeredIndex, notifySelected)
+				const STOP_SUBSCRIBERS = useSubscribers(
+					Toggleable.usePanel(node, {
+						listeners: ['FOCUS_LEAVE', 'ESCAPE_KEY', 'CLICK_OUTSIDE'],
+					}),
+					Navigable.useNavigation(node),
+					Navigable.usePlugins(node, useKeyMatch),
+					Navigable.listenSelected((selected) => {
+						node.setAttribute('aria-activedescendant', selected.id);
+					})
 				);
 
-				const selectNavIndex = handlers.handleSelection(registeredIndex);
-				node.addEventListener('click', selectNavIndex);
-				node.addEventListener('click', close);
+				node.id = menuName;
+				node.setAttribute('role', 'menu');
+				node.setAttribute('aria-labelledby', buttonName);
+				node.addEventListener('keydown', preventTabbing);
 				return {
-					destroy: () => {
-						StopIsSelected(), Items.unregister(node);
-						node.removeEventListener('click', selectNavIndex);
-						node.removeEventListener('click', close);
+					destroy() {
+						startWith = 'AUTO';
+						STOP_SUBSCRIBERS();
+						node.removeEventListener('keydown', preventTabbing);
 					},
 				};
 			},
-			button: (node: HTMLElement) => {
-				button = node;
-				const DisposeButton = useButton(node, 'ArrowUp', 'ArrowDown');
+			item(node: HTMLElement, notifySelected: Notifier<boolean>) {
+				const id = generateItemId.next().value as number;
+				const itemName = baptize('item', id);
+
+				const index = Items.register(node, removeFocusable);
+
+				const STOP_SUBSCRIBERS = useSubscribers(
+					useHoverSync(node, index, Navigable.interact),
+					Navigable.isSelected(index, notifySelected)
+				);
+
+				const handleKeys = ({ code }: KeyboardEvent) =>
+					['Enter', 'Space'].includes(code) && Toggleable.close();
+
+				node.id = itemName;
+				node.setAttribute('role', 'menuitem');
+				node.addEventListener('mousedown', Toggleable.close);
+				node.addEventListener('keydown', handleKeys);
 				return {
-					destroy: () => {
-						DisposeButton();
+					destroy() {
+						STOP_SUBSCRIBERS(), Items.unregister(node, index);
+						node.removeEventListener('mousedown', Toggleable.close);
+						node.removeEventListener('keydown', handleKeys);
 					},
 				};
 			},
@@ -88,42 +115,30 @@
 	}
 
 	export const isMenuContext = (val: unknown): val is MenuContext =>
-		propsIn(val, 'Open', 'close', 'menu', 'item', 'button');
+		propsIn(val, 'Open', 'button', 'menu', 'item');
 
-	interface MenuContext extends Pick<Toggleable, 'close'> {
+	interface MenuContext {
 		Open: Readable<boolean>;
-		menu: (node: HTMLElement, styles?: SelectedStyles) => void;
-		item: (node: HTMLElement, notifySelected: Notifier<boolean>) => void;
 		button: (node: HTMLElement) => void;
+		menu: (node: HTMLElement, activeClass?: string) => void;
+		item: (node: HTMLElement, notifySelected: Notifier<boolean>) => void;
 	}
 
-	interface MenuSetttings {
+	interface MenuSettings {
 		Toggleable: Toggleable;
 	}
 </script>
 
 <script lang="ts">
 	import { setContext } from 'svelte';
-	import { derived } from 'svelte/store';
-	import {
-		makeFocusable,
-		removeFocusable,
-		useFocusTrap,
-	} from '$lib/utils/focus-management';
+	import { toReadable } from '$lib/utils';
 
 	let open = false;
-	const Toggleable = toggleable(open, (bool) => (open = bool));
+	let Toggleable = toggleable(open, (bool) => (open = bool), true);
+	const Open = toReadable(Toggleable);
 
 	const { button, menu, item } = initMenu({ Toggleable });
-	const Context = {
-		Open: derived(Toggleable, ($Open) => $Open),
-		close: Toggleable.close,
-		button,
-		menu,
-		item,
-	};
-
-	setContext(MENU_CONTEXT_KEY, Context);
+	setContext(MENU_CONTEXT_KEY, { Open, button, menu, item });
 </script>
 
 <slot {open} {button} {menu} />
